07822f7522d691f4c78a2f42227338601cec903a
Faizan <marral7654321@gmail.com>
2026-06-25 11:42:04 +0500

    cros orgin issue resolving

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,11 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://bvolox.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://bvolox.vercel.app",
+      "https://bvolox-p54xzsmie-faizans-projects-89f12db9.vercel.app"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
